6be43e573a9b8d4228de938489a5eceb0b7899c0
Malik <malikfelix50@gmail.com>
2023-08-16 22:16:04 -0500

    remove form control

--- src/Components/Forms/ContactForm.jsx
+++ src/Components/Forms/ContactForm.jsx
@@ -1,50 +1,47 @@
 import { TextField } from "@mui/material";
 import PrimaryButton from "../Buttons/PrimaryButton";
-import { FormControl } from "@mui/material";
 
 const ContactForm = () => {
   return (
     <div className="contact-form-container">
-      <FormControl>
-        <form
-          method="POST"
-          name="contact-mui"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact-mui" />
-          <TextField
-            label="Name"
-            name="name"
-            required
-            fullWidth
-            type="text"
-            margin="normal"
-          />
-          <TextField
-            label="Email Address"
-            required
-            fullWidth
-            type="email"
-            margin="normal"
-          />
-          <TextField
-            label="Company/Organization Name"
-            fullWidth
-            type="text"
-            margin="normal"
-          />
-          <TextField
-            label="Additional Comments/Notes"
-            fullWidth
-            multiline
-            rows={4}
-            type="text"
-            margin="normal"
-          />
-          <PrimaryButton type="submit">Submit</PrimaryButton>
-        </form>
-      </FormControl>
+      <form
+        method="POST"
+        name="contact-mui"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact-mui" />
+        <TextField
+          label="Name"
+          name="name"
+          required
+          fullWidth
+          type="text"
+          margin="normal"
+        />
+        <TextField
+          label="Email Address"
+          required
+          fullWidth
+          type="email"
+          margin="normal"
+        />
+        <TextField
+          label="Company/Organization Name"
+          fullWidth
+          type="text"
+          margin="normal"
+        />
+        <TextField
+          label="Additional Comments/Notes"
+          fullWidth
+          multiline
+          rows={4}
+          type="text"
+          margin="normal"
+        />
+        <PrimaryButton type="submit">Submit</PrimaryButton>
+      </form>
     </div>
   );
 };
