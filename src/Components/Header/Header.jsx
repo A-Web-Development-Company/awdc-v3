4c6257cb988c9d36409afa721b381c5df39fe5b2
Malik <malikfelix50@gmail.com>
2024-06-05 22:19:58 -0500

    remove visibility of main nav contact button on mobile and display menu on half of the mobile viewport

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PrimaryButton from "../Buttons/PrimaryButton";
 import Logo from "../../assets/images/awdc-logo-lower-spaced-no-background.png";
 import { Link } from "react-router-dom";
@@ -9,6 +9,7 @@ import "./Header.css";
 const Header = () => {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+  const navMenuRef = useRef(null);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -21,65 +22,79 @@ const Header = () => {
     }
   };
 
+  const handleOutsideClick = (e) => {
+    if (navMenuRef.current && !navMenuRef.current.contains(e.target)) {
+      closeMobileMenu();
+    }
+  };
+
   useEffect(() => {
     showButton();
+    window.addEventListener("resize", showButton);
+    window.addEventListener("click", handleOutsideClick);
+    return () => {
+      window.removeEventListener("resize", showButton);
+      window.removeEventListener("click", handleOutsideClick);
+    };
   }, []);
 
   // below might be more efficient with tailwind implementation
-  // sets simple media query-like conditional display once 
+  // sets simple media query-like conditional display once
   // instead running func at any/all page adjustment
   // may not be necessary as most page size changes are automatic
   window.addEventListener("resize", showButton);
 
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="left">
-          <div className="logo">
-            <Link to='/'><img src={Logo} alt="awdc-logo" /></Link>
+    <>
+      <nav className="navbar">
+        <div className="navbar-container" ref={navMenuRef}>
+          <div className="left">
+            <div className="logo">
+              <Link to="/">
+                <img src={Logo} alt="awdc-logo" />
+              </Link>
+            </div>
+            <div className="menu-icon" onClick={handleClick}>
+              <FontAwesomeIcon icon={click ? faTimes : faBars} />
+            </div>
           </div>
-          <div className="menu-icon" onClick={handleClick}>
-            <FontAwesomeIcon icon={click ? faTimes : faBars} />
+          <div className="right">
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link className="nav-link" to="/" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/About"
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <PrimaryButton
+                  to="/"
+                  className="mobile-contact-button"
+                  onClick={closeMobileMenu}
+                >
+                  Contact
+                </PrimaryButton>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="right">
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={closeMobileMenu}>
-                Work
-              </Link>
-            </li> */}
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={closeMobileMenu}>
-                Services
-              </Link>
-            </li> */}
-            <li className="nav-item">
-              <Link className="nav-link" to="/About" onClick={closeMobileMenu}>
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <PrimaryButton
-                to="/"
-                className="mobile-contact-button"
-                onClick={closeMobileMenu}
-              >
-                Contact
-              </PrimaryButton>
-            </li>
-          </ul>
+        <div className="contact-nav-button">
+          <PrimaryButton className="contact-cta-button">Contact</PrimaryButton>
         </div>
-      </div>
-      <div className="contact-nav-button">
-        <PrimaryButton className="contact-cta-button">Contact</PrimaryButton>
-      </div>
-    </nav>
+      </nav>
+      <div
+        className={`overlay ${click ? "active" : ""}`}
+        onClick={closeMobileMenu}
+      ></div>
+    </>
   );
 };
 
