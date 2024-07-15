3a28080010f2d1f4483a5c0cfe1fd9c4fa038bfb
erreyonj <erreyon.js@gmail.com>
2024-07-15 10:21:49 -0500

    add mobile sizing tweaks

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -17,29 +17,27 @@ const AboutPage = () => {
 
       <div id='aboutStructure' className=''>
         <div id="headshotCol1" className='fixed left-0 border-r-zinc-200 w-18 shadow-lg'>
-          <div className="teamMember flex flex-col w-16 shadow-sm">
-            <img src={MalikHS} className=''/>
+          <div className="teamMember sm:w-24 lg:w-32 flex flex-col w-20 shadow-sm">
+            <a href=''><img src={MalikHS} className=''/></a>
             <h3 className='text-red'>Malik Felix</h3>
             <h4 className='text-center mx-auto hidden'>Co-Founder</h4>
             <p className='text-black hidden sm:visible'>My passion lies in Front End Development and UI Design, where I use my expertise to build websites and web applications that look great and function well.</p>
           </div>
-          <div className="teamMember w-16 shadow-sm">
-            <img src={ErreyonHS} className=''/>
+          <div className="teamMember sm:w-24 lg:w-32 w-20 shadow-sm">
+            <a href=''><img src={ErreyonHS} className=''/></a>
             <h3>Erreyon Elders</h3>
             <h4 className='text-center mx-auto hidden'>Founding Developer</h4>
             <p className='hidden sm:visible'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, consectetur.</p>
           </div>
 
-          {/* On mobile, the following headshots scroll on the left column*/}
-
-          <div className="teamMember shortList w-16 shadow-sm">
-            <img src={EssoHS} className=''/>
+          <div className="teamMember sm:w-24 lg:w-32 w-20 shadow-sm">
+            <a href=''><img src={EssoHS} className=''/></a>
             <h3>Esso Bodjana</h3>
             <p className='hidden sm:visible'>I was born and raised in NYC and recently moved to San Diego to take advantage of the sunny weather. I bring 10+ years of experience working in the finance, tech, and start up space. Very interested in helping people find their potential.</p>
           </div>
         </div>
 
-        <div id="statementCol" className='overflow-hidden ml-16'>
+        <div id="statementCol" className='overflow-hidden ml-24 lg:ml-28'>
           <div id='statementContainer' className='overflow-scroll'>
             <div className="statement">
               <h2>Mission</h2>
@@ -63,19 +61,6 @@ const AboutPage = () => {
               <a href='' className='shadow-sm'><FaTwitter /></a>
               <a href='' className='shadow-sm'><FaInstagram /></a>
             </div>
-          </div>
-        </div>
-
-        <div id="headshotCol2" className="bg-slate-600">
-          <div className="teamMember">
-            <img src={EssoHS} className=''/>
-            <h3>Esso Bodjana</h3>
-            <p>I was born and raised in NYC and recently moved to San Diego to take advantage of the sunny weather. I bring 10+ years of experience working in the finance, tech, and start up space. Very interested in helping people find their potential.</p>
-          </div>
-          <div className="teamMember">
-            <img src={PlaceholderHS} className=''/>
-            <h3>Name</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, consectetur.</p>
           </div>
         </div>
       </div>
